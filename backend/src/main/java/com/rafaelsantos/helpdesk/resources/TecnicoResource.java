4dab35f8306a68eefcc166c3c99ab36e4d76866b
Rafael Santos <rafa.05125@gmail.com>
2023-01-10 19:37:52 -0300

    Tecnico DTO

--- backend/src/main/java/com/rafaelsantos/helpdesk/resources/TecnicoResource.java
+++ backend/src/main/java/com/rafaelsantos/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelsantos.helpdesk.domain.Tecnico;
+import com.rafaelsantos.helpdesk.domain.dto.TecnicoDTO;
 import com.rafaelsantos.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = this.service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
