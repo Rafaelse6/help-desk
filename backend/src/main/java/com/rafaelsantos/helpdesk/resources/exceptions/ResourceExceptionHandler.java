dbb171c6ea573e7087722e613fbd9f99e177e65e
Rafael Santos <rafa.05125@gmail.com>
2023-01-11 23:45:37 -0300

    Object not found exception

---
+++ backend/src/main/java/com/rafaelsantos/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.rafaelsantos.helpdesk.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.rafaelsantos.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Object not found", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+}
--- backend/src/main/java/com/rafaelsantos/helpdesk/services/TecnicoService.java
+++ backend/src/main/java/com/rafaelsantos/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rafaelsantos.helpdesk.domain.Tecnico;
 import com.rafaelsantos.helpdesk.repositories.TecnicoRepository;
+import com.rafaelsantos.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -15,6 +16,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id));
 	}
 }
