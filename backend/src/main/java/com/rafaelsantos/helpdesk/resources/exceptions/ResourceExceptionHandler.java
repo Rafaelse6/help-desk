6f162b00cd3375731f4dd010aebdab33e7132a2b
Rafael Santos <rafa.05125@gmail.com>
2023-01-14 10:57:47 -0300

    Data integrity violation exception

--- backend/src/main/java/com/rafaelsantos/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/rafaelsantos/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rafaelsantos.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.rafaelsantos.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,15 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		
 		StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Object not found", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(error);
 	}
--- backend/src/main/java/com/rafaelsantos/helpdesk/services/TecnicoService.java
+++ backend/src/main/java/com/rafaelsantos/helpdesk/services/TecnicoService.java
@@ -6,15 +6,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rafaelsantos.helpdesk.domain.Pessoa;
 import com.rafaelsantos.helpdesk.domain.Tecnico;
 import com.rafaelsantos.helpdesk.domain.dto.TecnicoDTO;
+import com.rafaelsantos.helpdesk.repositories.PessoaRepository;
 import com.rafaelsantos.helpdesk.repositories.TecnicoRepository;
+import com.rafaelsantos.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.rafaelsantos.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
 	@Autowired
 	private TecnicoRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -27,7 +33,20 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj); 
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já está cadastrado no sistema");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já está cadastrado no sistema");
+		}
 	}
 }
